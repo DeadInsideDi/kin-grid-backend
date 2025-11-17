4585d0a1281ffe0c358cb3938a84076eb0b9aeed
DeadInsideDi <matvei778990@gmail.com>
2025-11-17 17:37:52 +0300

    + hello check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { ScheduleModule } from '@nestjs/schedule'
+import { AppController } from './app.controller'
 import { AuthModule } from './auth/auth.module'
 import { FamilyInvitationModule } from './family-invitation/family-invitation.module'
 import { FamilyMemberModule } from './family-member/family-member.module'
 import { FamilyModule } from './family/family.module'
 import { MailerModule } from './mailer/mailer.module'
+import { PricingModule } from './pricing/pricing.module'
 import { RelationsModule } from './relations/relations.module'
 import { UserModule } from './user/user.module'
-import { PricingModule } from './pricing/pricing.module';
 
 @Module({
 	imports: [
@@ -25,5 +26,6 @@ import { PricingModule } from './pricing/pricing.module';
 		MailerModule,
 		PricingModule,
 	],
+	controllers: [AppController],
 })
 export class AppModule {}
