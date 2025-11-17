04c169c695040cfcddc6fe3922f3cf1139f36bb3
DeadInsideDi <matvei778990@gmail.com>
2025-11-17 19:45:31 +0300

    domain fix

--- src/config/storage.config.ts
+++ src/config/storage.config.ts
@@ -3,7 +3,7 @@ import { CookieOptions } from 'express'
 export const COOKEI = {
 	COOKIE_OPTIONS: {
 		httpOnly: true,
-		domain: process.env.FRONTEND_URL,
+		domain: process.env.DOMAIN,
 		secure: true,
 		sameSite: 'lax',
 	} as CookieOptions,
